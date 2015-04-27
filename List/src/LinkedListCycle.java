084e1e544b405615488a1c5a8e9c1dc06d2a5d18
feeshne <feeshne@gmail.com>
2015-04-27 11:50:58 +0800

    好经典的算法题，很有意思，不错， ps：加了点注释，用不用try-catch对算法运行时间影响不大

--- List/src/LinkedListCycle.java
+++ List/src/LinkedListCycle.java
@@ -20,7 +20,8 @@ public class LinkedListCycle {
     public static boolean hasCycle(ListNode head) {
         ListNode slow=head;
         ListNode fast=head;
-        while(slow!=null&&fast!=null){
+        while(slow!=null&&fast.next!=null){
+            //加不加try-catch语句对运行时间影响不大
             try{
                 slow=slow.next;
                 fast=fast.next.next;
