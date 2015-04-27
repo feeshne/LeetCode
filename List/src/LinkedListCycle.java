b63b14b3b80a4f5243c83b4ee1fa3085e3f0c650
feeshne <feeshne@gmail.com>
2015-04-27 11:48:28 +0800

    好经典的算法题，很有意思，不错

---
+++ List/src/LinkedListCycle.java
@@ -0,0 +1,44 @@
+/**
+ * Created by Feeshne on 2015/4/27.
+ */
+public class LinkedListCycle {
+    public static void main(String[] args) {
+        ListNode a=new ListNode(1);
+        ListNode b=new ListNode(2);
+        ListNode c=new ListNode(3);
+        ListNode d=new ListNode(4);
+        ListNode e=new ListNode(5);
+        ListNode f=new ListNode(6);
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=e;
+        e.next=f;
+//        f.next=b;
+        System.out.println(hasCycle(a));
+    }
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(slow!=null&&fast!=null){
+            try{
+                slow=slow.next;
+                fast=fast.next.next;
+            }catch (NullPointerException e){
+                return false;
+            }
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
--- Main/src/Main.java
+++ Main/src/Main.java
@@ -9,6 +9,9 @@ public class Main {
 //        A a=new SonOfA();
 //        a.howl();不能使用，只有A中有howl中才行 就是父类只有100容量，子类容量扩大到了200，子类放进父类，父类只能调用原来的100，其他100不能调用
         System.out.println(3%2);
+        A a=new A();
+        A b=new A();
+        System.out.println(a==b);
     }
 
 }
