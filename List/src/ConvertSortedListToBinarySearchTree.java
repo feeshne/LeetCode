bdb20c6da369bc17a3f10391a5728e13c327a755
feeshne <feeshne@gmail.com>
2015-05-14 16:18:24 +0800

    看到了一个较牛逼的算法，修改链表结构，但是省去了计算中间值，一次搞定中间点问题。牛逼。

--- List/src/ConvertSortedListToBinarySearchTree.java
+++ List/src/ConvertSortedListToBinarySearchTree.java
@@ -15,48 +15,89 @@ public class ConvertSortedListToBinarySearchTree {
         a.next = b;
         b.next = c;
         c.next = d;
-        d.next=e;
+        d.next = e;
         e.next = f;
         f.next = g;
-        TreeNode res=sortedListToBST(a);
+        TreeNode res = sortedListToBST(a);
         System.out.println(res.val);
     }
+
     public static TreeNode sortedListToBST(ListNode head) {
-        int length=0;
-        ListNode cur=head;
-        while(cur!=null){
-            cur=cur.next;
+        int length = 0;
+        ListNode cur = head;
+        while (cur != null) {
+            cur = cur.next;
             length++;
         }
-        return cal(head,0,length-1);
+        return cal(head, 0, length - 1);
+        //
+        /*感动不已的解法
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return new TreeNode(head.val);
+        }
+
+        ListNode mid = head;
+        ListNode pre_mid = null;
+        ListNode fast = head;
+
+        while (true) {
+            if (fast != null && fast.next != null) {
+                fast = fast.next.next;
+            } else {
+                break;
+            }
+            pre_mid = mid;
+            mid = mid.next;
+        }
+        if (pre_mid != null)
+            pre_mid.next = null;
+
+
+        TreeNode root = new TreeNode(mid.val);
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(mid.next);
+
+        return root;
+        */
     }
-    public static TreeNode cal(ListNode tmp,int pre,int last){
-        if(pre>last) {
+
+    public static TreeNode cal(ListNode tmp, int pre, int last) {
+        if (pre > last) {
             return null;
         }
 //        int mid=pre+(last-pre)/2;
-        int mid=(pre+last)/2;
-        ListNode tmp0=tmp;
-        for (int i=pre;i<mid;i++){
-            tmp0=tmp0.next;
+        int mid = (pre + last) / 2;
+        ListNode tmp0 = tmp;
+        for (int i = pre; i < mid; i++) {
+            tmp0 = tmp0.next;
         }
-        TreeNode left=cal(tmp,pre,mid-1);
-        TreeNode right=cal(tmp0.next,mid+1,last);
-        TreeNode res=new TreeNode(tmp0.val);
-        res.left=left;
-        res.right=right;
+        TreeNode left = cal(tmp, pre, mid - 1);
+        TreeNode right = cal(tmp0.next, mid + 1, last);
+        TreeNode res = new TreeNode(tmp0.val);
+        res.left = left;
+        res.right = right;
         return res;
     }
+
     public static class ListNode {
         int val;
         ListNode next;
-        ListNode(int x) { val = x; }
+
+        ListNode(int x) {
+            val = x;
+        }
     }
-    public static class TreeNode
-    {
+
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode(int x) { val = x; }
+
+        TreeNode(int x) {
+            val = x;
+        }
     }
 }
